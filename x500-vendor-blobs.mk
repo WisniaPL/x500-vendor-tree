c095e0d9bb33aaf1b5fa808398635bcce9c6b6d2
WisniaPL <19wisnia89@gmail.com>
2017-01-12 21:13:10 +0100

    Test

---
+++ x500-vendor-blobs.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeTV/x500/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeTV/x500/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeTV/x500/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeTV/x500/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeTV/x500/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeTV/x500/vendor,system/vendor)
+
+
