e1b3a05d3008f2085f99aade2e53f9e95e8cb574
WisniaPL <19wisnia89@gmail.com>
2017-03-13 01:49:34 +0100

    Beta

--- Android.mk
+++ Android.mk
@@ -1,34 +1,25 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),x500)
+$(info [Decker] copying DP Framework proprietary blobs)
 
-ifeq ($(USE_MTK_CAMERA_WRAPPER),true)
+
 include $(CLEAR_VARS)
-LOCAL_MODULE := libcam2halsensor
-LOCAL_SRC_FILES_64 := proprietary/lib64/libcam2halsensor.so
-LOCAL_SRC_FILES_32 := proprietary/lib/libcam2halsensor.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_OWNER = $VENDOR
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MULTILIB = 64
+LOCAL_SRC_FILES_64 = proprietary/lib64/libdpframework.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libcameracustom
-LOCAL_SRC_FILES_64 := proprietary/lib64/libcameracustom.so
-LOCAL_SRC_FILES_32 := proprietary/lib/libcameracustom.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
-endif
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libmtkcamera_client
-LOCAL_SRC_FILES_64 := proprietary/lib64/libmtkcamera_client.so
-LOCAL_SRC_FILES_32 := proprietary/lib/libmtkcamera_client.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_OWNER = mtk
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MULTILIB = 32
+LOCAL_SRC_FILES_32 = proprietary/lib/libdpframework.so
 include $(BUILD_PREBUILT)
 
-endif
